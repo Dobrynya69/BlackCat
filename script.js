47966815f1dd953161ee522bd0224e8017710382
Roman <roma040805@gmail.com>
2022-09-23 15:14:45 +0300

    0.3

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const scrollLinks = document.querySelectorAll('.header__link[data-to]');
 scrollLinks.forEach(scrollLink => {
     scrollLink.addEventListener('click', function (e) {
-        document.body.classList.remove('active_scroll');
+        document.body.classList.remove('active_scroll')
         document.querySelector('.header__menu-icon').classList.remove('active')
         document.querySelector('.header__menu').classList.remove('active')
         const block = document.getElementById(e.target.dataset.to)
-        block.scrollIntoView({behavior: "smooth"});
+        block.scrollIntoView({behavior: "smooth"})
     })
 })
 
@@ -29,3 +29,38 @@ if (contactButtons) {
 	    })
     })
 }
+
+const slides = document.querySelectorAll('.slide-img')
+slides.forEach(slide => {
+    slide.addEventListener('click', function (e) {
+        document.body.classList.add('active_scroll')
+        const bigImgs = document.querySelectorAll('.slide__big-img')
+        bigImgs.forEach(bigImg => {
+            if (bigImg.classList.contains(e.target.classList[1])) {  
+                if (bigImg.classList.contains("active")) {
+                    document.body.classList.remove('active_scroll')
+                    bigImg.classList.remove("active")
+                }
+                else{
+                    bigImgs.forEach(toRemove => {
+                        toRemove.classList.remove("active")
+                    })
+                    bigImg.classList.add("active")
+                }
+            }
+        })
+    })
+})
+const closes = document.querySelectorAll('.close')
+closes.forEach(close => {
+    close.addEventListener('click', function (e) {
+        
+        const bigImgs = document.querySelectorAll('.slide__big-img')
+        bigImgs.forEach(bigImg => {
+            if (bigImg.classList.contains(close.classList[1])) {  
+                document.body.classList.remove('active_scroll')
+                bigImg.classList.remove("active")
+            }
+        })
+    })
+})
